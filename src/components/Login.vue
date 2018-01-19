69145d97d8038d8a7972e8202684192ec616d2af
Sacha Bejaud <sacha.bejaud@digi-smart.fr>
2018-01-19 16:48:47 +0100

    Préparation pour vue-i18n et element-ui
    • Ajout des packages vue-i18n et element-ui
    • Nettoyage du code

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="login">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
