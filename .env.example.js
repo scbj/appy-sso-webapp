65978c966c9f8918fa0cc774a4000f81a2341728
Sacha Bejaud <sacha.bejaud@digi-smart.fr>
2018-01-19 11:59:20 +0100

    Mise en place d'un système de variables d'environement spécifique aux machine

---
+++ .env.example.js
@@ -0,0 +1,10 @@
+/**
+ * Environment variables specific to the machine.
+ * Copy the content of the .env.example.js file to the .env.js file also at the root of the project.
+ */
+
+export default {
+  api: {
+    baseURL: 'http://192.168.1.98/SACHA/appy-sso/public/'
+  }
+}
