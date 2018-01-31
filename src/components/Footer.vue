4e12e94ec17d235a375b928c11983fe6ac77ca7e
Sacha Bejaud <sacha.bejaud@digi-smart.fr>
2018-01-31 17:43:19 +0100

    Configuration de la langue dans le footer
    • Suppression du logo de Vuejs des assets
    • Ajout de la configuration de la langue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,17 @@
 <template lang="pug">
 .ay-footer
   .copyright {{ copyright }}
-  a.contact(href="#contact" v-text="$t('contact')")
-  a.about(href="#about" v-text="$t('about')")
-  a.terms(href="#terms" v-text="$t('terms')")
+  a.contact(href='#contact' v-text="$t('contact')")
+  a.about(href='#about' v-text="$t('about')")
+  a.terms(href='#terms' v-text="$t('terms')")
+  el-dropdown.language
+    span.el-dropdown-link
+      img(src='/static/img/world.svg')
+      | Français
+      i.el-icon-arrow-down.el-icon--right
+    el-dropdown-menu(slot='dropdown')
+      el-dropdown-item Français
+      el-dropdown-item(disabled) English
 </template>
 
 <i18n>
@@ -38,17 +46,36 @@ export default {
   color: #909399;
   padding: 2rem;
   display: grid;
-  grid-template-columns: auto 1fr auto auto auto;
-  grid-template-areas: "copyright . contact about terms";
+  grid-template-columns: auto 1fr auto auto auto auto;
+  grid-template-areas: "copyright . contact about terms language";
   grid-column-gap: 1rem;
-}
 
-.copyright { grid-area: copyright; }
-.contact { grid-area: contact; }
-.about { grid-area: about; }
-.terms { grid-area: terms; }
+  .copyright { grid-area: copyright; }
+  .contact { grid-area: contact; }
+  .about { grid-area: about; }
+  .terms { grid-area: terms; }
+  .language { grid-area: language; }
+}
 
 a:hover {
     color: #409EFF;
+}
+
+.language {
+  color: #909399;
+  cursor: pointer;
+
+  img {
+    opacity: .4;
+    margin-right: .5em;
+  }
+
+  .el-dropdown-link {
+    display: flex
+  }
+
+  i {
+    align-self: center;
+  }
 }
 </style>
