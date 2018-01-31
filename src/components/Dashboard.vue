7f8d23f332053a65891df86e5d25eaea72f81e5e
Sacha Bejaud <sacha.bejaud@digi-smart.fr>
2018-01-31 12:09:55 +0100

    Création du composant Dashboard et de sa route

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .dashboard Hello world!
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import Dashboard from '@/components/Dashboard.vue'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard
     }
   ]
 })
